b89a96366c5f2f4b886ea3cd42940fe09eb33632
Nathan Phan <nathphan510@gmail.com>
2019-03-23 23:53:24 -0700

    Linked api and profile api

--- controllers/api.js
+++ controllers/api.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 
+const port = process.env.PORT || 5000;
+
 exports.api_get_username = (req, res, next) => {
     const params = {
         headers: {
@@ -13,8 +15,36 @@ exports.api_get_username = (req, res, next) => {
     axios.get(url, params)
         .then(response => {
             let data = response.data;
-            console.log(data);
-            res.json(data);
+            // console.log(data);
+            const obj = createProfile(data);
+            
+            axios.post(`http://localhost:${port}/profile`, obj)
+                .then(response => {
+                    console.log(response.data);
+                    res.json(response.data);
+                })
+                .catch(err => console.log(err));
         })
         .catch(err => console.log(err));
+}
+
+function createProfile(data) {
+    const obj = {};
+    obj.username = data.data.metadata.platformUserHandle;
+    obj.level = data.data.metadata.level;
+    obj.totalKills = data.data.stats[1].value;
+    obj.champions = [];
+    data.data.children.forEach(child => {
+        let newChild = {};
+        newChild.legend = child.metadata.legend_name;
+        newChild.stats = [];
+        child.stats.forEach(stat => {
+            let newStat = {};
+            newStat.statName = stat.metadata.name;
+            newStat.statValue = stat.value;
+            newChild.stats.push(newStat);
+        })
+        obj.champions.push(newChild);
+    });
+    return obj;
 }
